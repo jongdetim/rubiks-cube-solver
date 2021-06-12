e374225661e6012234f88b5a7efd1b1704310984
Tim De jong <tide-jon@f1r4s3.codam.nl>
2021-06-12 21:09:39 +0200

    ding

---
+++ src/database/generate_db.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   generate_db.cpp                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tide-jon <tide-jon@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/06/12 19:02:30 by tide-jon      #+#    #+#                 */
+/*   Updated: 2021/06/12 19:15:09 by tide-jon      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+// using namespace std;
+
+enum edge {uf, ur, ub, ul, df, dr, db, dl, fr, br, bl, fl};
+
+int main(){
+    std::cout << ur;
+    return 0;
+}
