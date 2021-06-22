93805dfc63a710221a4dc1f13e513982b5bbaae6
aidan <asulliva@f1r6s15.codam.nl>
2021-06-22 20:30:03 +0200

    added edgePosition

--- src/moves.cpp
+++ src/moves.cpp
@@ -6,7 +6,7 @@
 /*   By: asulliva <asulliva@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/21 20:14:57 by asulliva      #+#    #+#                 */
-/*   Updated: 2021/06/22 19:06:32 by asulliva      ########   odam.nl         */
+/*   Updated: 2021/06/22 20:29:31 by asulliva      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,12 @@ Cube &Cube::u(uint8_t amount)
 		this->cornerOrientation[CORNER::UBR] = this->cornerOrientation[CORNER::ULB];
 		this->cornerOrientation[CORNER::ULB] = this->cornerOrientation[CORNER::UFL];
 		this->cornerOrientation[CORNER::UFL] = temp;
+
+		EDGE eTemp = this->edgePosition[EDGE::UB];
+		this->edgePosition[EDGE::UB] = this->edgePosition[EDGE::UL];
+		this->edgePosition[EDGE::UL] = this->edgePosition[EDGE::UF];
+		this->edgePosition[EDGE::UF] = this->edgePosition[EDGE::UR];
+		this->edgePosition[EDGE::UR] = eTemp;
 	}
 	return *this;
 }
@@ -60,6 +66,12 @@ Cube &Cube::l(uint8_t amount)
 		this->cornerOrientation[CORNER::UFL] = (2 + this->cornerOrientation[CORNER::ULB]) % 3;
 		this->cornerOrientation[CORNER::ULB] = (1 + this->cornerOrientation[CORNER::DBL]) % 3;
 		this->cornerOrientation[CORNER::DBL] = (2 + temp) % 3;
+
+		EDGE eTemp = this->edgePosition[EDGE::BL];
+		this->edgePosition[EDGE::BL] = this->edgePosition[EDGE::DL];
+		this->edgePosition[EDGE::DL] = this->edgePosition[EDGE::FL];
+		this->edgePosition[EDGE::FL] = this->edgePosition[EDGE::UL];
+		this->edgePosition[EDGE::UL] = eTemp;
 	}
 	return *this;
 }
@@ -86,6 +98,12 @@ Cube &Cube::f(uint8_t amount)
 		this->cornerOrientation[CORNER::UFL] = (1 + this->cornerOrientation[CORNER::DLF]) % 3;
 		this->cornerOrientation[CORNER::DLF] = (2 + this->cornerOrientation[CORNER::DFR]) % 3;
 		this->cornerOrientation[CORNER::DFR] = (1 + temp) % 3;
+
+		EDGE eTemp = this->edgePosition[EDGE::UF];
+		this->edgePosition[EDGE::UF] = this->edgePosition[EDGE::FL];
+		this->edgePosition[EDGE::FL] = this->edgePosition[EDGE::DF];
+		this->edgePosition[EDGE::DF] = this->edgePosition[EDGE::FR];
+		this->edgePosition[EDGE::FR] = eTemp;
 	}
 	return *this;
 }
@@ -112,6 +130,12 @@ Cube &Cube::r(uint8_t amount)
 		this->cornerOrientation[CORNER::DFR] = (2 + this->cornerOrientation[CORNER::DRB]) % 3;
 		this->cornerOrientation[CORNER::DRB] = (1 + this->cornerOrientation[CORNER::UBR]) % 3;
 		this->cornerOrientation[CORNER::UBR] = (2 + temp) % 3;
+
+		EDGE eTemp = this->edgePosition[EDGE::FR];
+		this->edgePosition[EDGE::FR] = this->edgePosition[EDGE::DR];
+		this->edgePosition[EDGE::DR] = this->edgePosition[EDGE::BR];
+		this->edgePosition[EDGE::BR] = this->edgePosition[EDGE::UR];
+		this->edgePosition[EDGE::UR] = eTemp;
 	}
 	return *this;
 }
@@ -119,9 +143,9 @@ Cube &Cube::r(uint8_t amount)
 /*
 ** Back turns
 */
-Cube&       Cube::b(uint8_t amount)
+Cube&		Cube::b(uint8_t amount)
 {
-	int     arr[] = {2, 1, 0, 8, 15, 14, 46, 45, 44, 28, 27, 26};
+	int		arr[] = {2, 1, 0, 8, 15, 14, 46, 45, 44, 28, 27, 26};
 	for (uint8_t i = 0; i < amount; i++)
 	{
 		this->rot90(FACE::BACK);
@@ -138,6 +162,12 @@ Cube&       Cube::b(uint8_t amount)
 		this->cornerOrientation[CORNER::UBR] = (1 + this->cornerOrientation[CORNER::DRB]) % 3;
 		this->cornerOrientation[CORNER::DRB] = (2 + this->cornerOrientation[CORNER::DBL]) % 3;
 		this->cornerOrientation[CORNER::DBL] = (1 + temp) % 3;
+
+		EDGE eTemp = this->edgePosition[EDGE::BR];
+		this->edgePosition[EDGE::BR] = this->edgePosition[EDGE::DB];
+		this->edgePosition[EDGE::DB] = this->edgePosition[EDGE::BL];
+		this->edgePosition[EDGE::BL] = this->edgePosition[EDGE::UB];
+		this->edgePosition[EDGE::UB] = eTemp;
 	}
 	return *this;
 }
@@ -145,9 +175,9 @@ Cube&       Cube::b(uint8_t amount)
 /*
 ** Back turns
 */
-Cube&       Cube::d(uint8_t amount)
+Cube&		Cube::d(uint8_t amount)
 {
-	int     arr[] = {14, 13, 12, 22, 21, 20, 30, 29, 28, 38, 37, 36};
+	int		arr[] = {14, 13, 12, 22, 21, 20, 30, 29, 28, 38, 37, 36};
 	for (uint8_t i = 0; i < amount; i++)
 	{
 		this->rot90(FACE::DOWN);
@@ -164,6 +194,12 @@ Cube&       Cube::d(uint8_t amount)
 		this->cornerOrientation[CORNER::DLF] = this->cornerOrientation[CORNER::DBL];
 		this->cornerOrientation[CORNER::DBL] = this->cornerOrientation[CORNER::DRB];
 		this->cornerOrientation[CORNER::DRB] = temp;
+
+		EDGE eTemp = this->edgePosition[EDGE::DF];
+		this->edgePosition[EDGE::DF] = this->edgePosition[EDGE::DL];
+		this->edgePosition[EDGE::DL] = this->edgePosition[EDGE::DB];
+		this->edgePosition[EDGE::DB] = this->edgePosition[EDGE::DR];
+		this->edgePosition[EDGE::DR] = eTemp;
 	}
 	return *this;
 }
