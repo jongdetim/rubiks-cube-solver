9d49f68250db648b2531202f34fb8573d7d3b62d
Tim De jong <tide-jon@f1r6s2.codam.nl>
2021-07-25 21:48:16 +0200

    cube rendering correctly with 9 char colour input

--- sfml-cube.cpp
+++ sfml-cube.cpp
@@ -1,9 +1,17 @@
+#define GL_SILENCE_DEPRECATION
+
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
-#include <SFML/graphics.hpp>
-#include <SFML/system.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <OpenGL/glext.h>
+#include <GLUT/glut.h>
 #include <cmath>
 #include <iostream>
+
+// clang++ sfml-cube.cpp -I /Users/tide-jon/.brew/Cellar/sfml/2.5.1_1/include -L /Users/tide-jon/.brew/Cellar/sfml/2.5.1_1/lib/ -lsfml-window -lsfml-graphics -lsfml-system -lsfml-audio -lsfml-network -framework OpenGL -framework GLUT
 
 void perspectiveGL( GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar )
 {
@@ -17,10 +25,89 @@ void perspectiveGL( GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFa
     glFrustum( -fW, fW, -fH, fH, zNear, zFar );
 }
 
+void setColour(char colour)
+{
+    switch ((char)colour)
+    {
+        case 'Y':
+            glColor3f(1,1,0);
+            break ;
+        case 'B':
+            glColor3f(0, 0, 1);
+            break ;
+        case 'R':
+            glColor3f(1,0,0);
+            break;
+        case 'G':
+            glColor3f(0,1,0);
+            break;
+        case 'O':
+            glColor3f(1.0f, 0.5f, 0.0f);
+            break;
+        case 'W':
+            glColor3f(1,1,1);
+            break;
+        default:
+            glColor3f(0,1,1);
+            break ;
+    }
+}
+
+void rotateFace(double x, double y, double z, char face)
+{
+    double theta = 1.5707963268;
+    switch (face)
+    {
+        case 'B':
+            theta *= 2;
+            glVertex3f(x*cos(theta) - z*sin(theta), y, x*sin(theta) + z*cos(theta));
+            break ;
+        case 'F':
+            glVertex3f(x, y, z);
+            break ;
+        case 'R':
+            theta *= -1;
+            glVertex3f(x*cos(theta) - z*sin(theta), y, x*sin(theta) + z*cos(theta));
+            break ;
+        case 'L':
+            glVertex3f(x*cos(theta) - z*sin(theta), y, x*sin(theta) + z*cos(theta));
+            break ;
+        case 'U':
+            glVertex3f(x, y*cos(theta) - z*sin(theta), y*sin(theta) + z*cos(theta));
+            break ;
+        case 'D':
+            theta *= -1;
+            glVertex3f(x, y*cos(theta) - z*sin(theta), y*sin(theta) + z*cos(theta));
+            break ;
+
+
+
+    }
+}
+
+void drawFace(std::string stickers, char face)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        // setColour(stickers[i]);
+        // rotateFace( 100 * ((i % 3) / 3) - 50, 50 - (100 * ((i / 3) / 3)), 50.f, face);
+        // rotateFace( 100 * ((i % 3) / 3) - 50, 50 - (100 * (((i / 3) + 1) / 3)), 50.f, face);
+        // rotateFace( 100 * (((i % 3) + 1) / 3) - 50, 50 - (100 * (((i / 3) + 1) / 3)), 50.f, face);
+        // rotateFace( 100 * (((i % 3) + 1) / 3) - 50, 50 - (100 * ((i / 3) / 3)), 50.f, face);
+
+        setColour(stickers[i]);
+        rotateFace( 100.f * (float((i % 3) + 1) / 3) - 50, 50 - (100.f * (float((i / 3) + 1) / 3)), 50.f, face);
+        rotateFace( 100.f * (float((i % 3) + 1) / 3) - 50, 50 - (100.f * (float(i / 3) / 3)), 50.f, face);
+        rotateFace( 100.f * (float(i % 3) / 3) - 50, 50 - (100.f * (float(i / 3) / 3)), 50.f, face);
+        rotateFace( 100.f * (float(i % 3) / 3) - 50, 50 - (100.f * (float((i / 3) + 1) / 3)), 50.f, face);
+        // exit(1);
+    }
+}
+
 int main()
 {
     // create the window
-    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+    sf::Window window(sf::VideoMode(800, 600), "Rubik", sf::Style::Default, sf::ContextSettings(32));
     window.setVerticalSyncEnabled(true);
 
     // activate the window
@@ -35,21 +122,26 @@ int main()
 
         //prepare OpenGL surface for HSR
 	glClearDepth(1.f);
-    glClearColor(0.3f, 0.3f, 0.3f, 0.f);
+    glClearColor(0.f, 0.f, 0.f, 0.f);
     glEnable(GL_DEPTH_TEST);
     glDepthMask(GL_TRUE);
 
     // sf::View view1(sf::FloatRect(200.f, 200.f, 300.f, 200.f));
-    sf::View view1;
-    view1.reset(sf::FloatRect(0, 0, 800, 600));
+    // sf::View view1;
+    // view1.reset(sf::FloatRect(0, 0, 800, 600));
     // view1.setRotation(10.f);
     //// Setup a perspective projection & Camera position
     glMatrixMode(GL_PROJECTION);
-    glLoadMatrixf(view1.getTransform().getMatrix());
-    // std::cout << *(view1.getTransform().getMatrix()) << std::endl;
-    glLoadIdentity();
-    perspectiveGL(90.f, 1.f, 100.f, 300.0f);//fov, aspect, zNear, zFar
     // glLoadMatrixf(view1.getTransform().getMatrix());
+    // std::cout << *(view1.getTransform().getMatrix()) << std::endl;
+    // glLoadIdentity();
+    // perspectiveGL(90.f, 1.f, 200.f, 300.0f);//fov, aspect, zNear, zFar
+    // glLoadMatrixf(view1.getTransform().getMatrix());
+    glLoadIdentity();
+    perspectiveGL(90.f, 1.f, 1.f, 300.0f);//fov, aspect, zNear, zFar
+    // glLoadMatrixf(view1.getTransform().getMatrix());
+        // glLoadIdentity();
+
 
 
     // load resources, initialize the OpenGL states, ...
@@ -62,15 +154,17 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            switch(event.type)
             {
-                // end the program
-                running = false;
-            }
-            else if (event.type == sf::Event::Resized)
-            {
-                // adjust the viewport when the window is resized
-                glViewport(0, 0, event.size.width, event.size.height);
+                case sf::Event::KeyPressed:
+                    if(event.key.code != sf::Keyboard::Escape) { break; }
+                case sf::Event::Closed:
+                    running = false;
+                    break;
+                case sf::Event::Resized:
+                    glViewport(0, 0, event.size.width, event.size.height);
+                default:
+                    break;
             }
         }
 
@@ -78,10 +172,13 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Apply some transformations for the cube
+            // glLoadMatrixf(view1.getTransform().getMatrix());
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         glTranslatef(0.f, 0.f, -200.f);
-		
+        gluLookAt(1,1,0, 0,0,1, 0,1.f,0);
+
+
 		if(rotate)
 			angle=Clock.getElapsedTime().asSeconds();
 		// glRotatef(angle * 30, 1.f, 0.f, 0.f);
@@ -90,41 +187,39 @@ int main()
 
         // draw...
         glBegin(GL_QUADS);//draw some squares
-            glColor3f(0,1,1); //cyan
-            glVertex3f(-50.f, -50.f, -50.f);
-            glVertex3f(-50.f,  50.f, -50.f);
-            glVertex3f( 50.f,  50.f, -50.f);
-            glVertex3f( 50.f, -50.f, -50.f);
+            // for (int i = 0; i < 9; i++)
+            // {
+            //     glColor3f(0,1,1); //cyan
+            //     glVertex3f( 50/3, 50.f, -50.f);
+            //     glVertex3f( 50/3,  50/3, -50.f);
+            //     glVertex3f( 50.f, 50/3, -50.f);
+            //     glVertex3f( 50.f,  50.f, -50.f);
+            // }
 
-            glColor3f(0,0,1); //blue
-            glVertex3f( 50.f, -50.f, 50.f);
-            glVertex3f( 50.f,  50.f, 50.f);
-            glVertex3f(-50.f,  50.f, 50.f);
-            glVertex3f(-50.f, -50.f, 50.f);
+            std::string stickers = "YORBGWYOR";
+            drawFace(stickers, 'F');
 
-            glColor3f(1,0,1); //magenta
-            glVertex3f(-50.f, -50.f,  50.f);
-            glVertex3f(-50.f,  50.f,  50.f);
-            glVertex3f(-50.f,  50.f, -50.f);
-            glVertex3f(-50.f, -50.f, -50.f);
+            // glColor3f(0,0,1); //blue
+            // glVertex3f( 50.f, -50.f, 50.f);
+            // glVertex3f( 50.f,  50.f, 50.f);
+            // glVertex3f(-50.f,  50.f, 50.f);
+            // glVertex3f(-50.f, -50.f, 50.f);
+            stickers = "CBBBCCCBC";
+            drawFace(stickers, 'B');
 
-            glColor3f(0,1,0); //green
-            glVertex3f(50.f, -50.f, -50.f);
-            glVertex3f(50.f,  50.f, -50.f);
-            glVertex3f(50.f,  50.f,  50.f);
-            glVertex3f(50.f, -50.f,  50.f);
+            stickers = "RRRRRRRRR";
+            drawFace(stickers, 'R');
 
-            glColor3f(1,1,0); //yellow
-            glVertex3f(-50.f, -50.f,  50.f);
-            glVertex3f(-50.f, -50.f, -50.f);
-            glVertex3f( 50.f, -50.f, -50.f);
-            glVertex3f( 50.f, -50.f,  50.f);
+            stickers = "WWWWWWWWW";
+            drawFace(stickers, 'U');
 
-            glColor3f(1,0,0); //red
-            glVertex3f( 50.f, 50.f,  50.f);
-            glVertex3f( 50.f, 50.f, -50.f);
-            glVertex3f(-50.f, 50.f, -50.f);
-            glVertex3f(-50.f, 50.f,  50.f);
+            stickers = "YYYYYYYYY";
+            drawFace(stickers, 'D');
+
+            stickers = "OOOOOOOOO";
+            drawFace(stickers, 'L');
+
+
         glEnd();
 
         // end the current frame (internally swaps the front and back buffers)
