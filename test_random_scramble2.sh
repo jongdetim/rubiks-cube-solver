70386afc13c5dcb5400122fe58a20bd36d4e5b8f
tim <timdedenker@gmail.com>
2021-07-30 19:48:42 +0200

    cleanup

--- test_random_scramble2.sh
+++ test_random_scramble2.sh
@@ -7,7 +7,7 @@ NC='\033[0m'
 for i in {0..100}
 do
 	    echo -e "$GREEN\nepoch $i\n$NC"
-	        ./rubik "" -v
+	        ./rubik.exe "" -v
 		    if [ $? -eq 139 ]; then echo "$RED Segfault$NC"; exit 1; fi
 		        sleep 1 #sleep for 1 sec cause time is used in random generate
 done
